854b0a3fa6a02ac22bdf4b266a714bd533842fc4
spark-156 <lucabergman@hotmail.nl>
2021-01-26 00:48:33 +0100

    :construction: Testing api backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [apiCallResponse, setApiCallResponse] = useState(null);
+  const [selectedApi, setSelectedApi] = useState('/api/helloworld');
 
   useEffect(() => {
     async function fetchApi() {
-      const res = await fetch('/helloworld');
+      const res = await fetch(selectedApi);
       const body = await res.json();
       if (res.status !== 200) {throw Error(body.message)}
       setApiCallResponse(body.string);
-      console.log(apiCallResponse)
     }
     fetchApi();
-  }, [])
+  }, [selectedApi]);
+
+  function handleApiSelectorChange({ target }) {
+    setSelectedApi(target.value);
+    console.log(selectedApi);
+  }
 
   return (
     <div>
       <h1>Welcome to React!</h1>
       <div>{apiCallResponse}</div>
+      <select onChange={handleApiSelectorChange}>
+        <option value="/api/helloworld">helloworld</option>
+        <option value="/api/express_backend">express</option>
+      </select>
     </div>
   );
 }
