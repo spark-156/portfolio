f4bfb83961aab785d5be80aee283b531e07cdb53
spark-156 <lucabergman@hotmail.nl>
2021-01-26 00:38:47 +0100

    :art: improved backend api url's

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Portfolio web-app listening at http://localhost:${port}`));
 
 // Hello word 
-app.get('/helloworld', (req, res) => {
+app.get('/api/helloworld', (req, res) => {
     res.send({ string: 'Hello World!' });
 });
 
 // Express backend is up and running
-app.get('/express_backend', (req, res) => {
-    res.send({ express: 'Yeah it is!' });
+app.get('/api/express_backend', (req, res) => {
+    res.send({ string: 'Yeah it is!' });
 });
