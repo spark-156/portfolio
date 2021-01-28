59274c1450a853877ace843bbd4173f042ae0a2e
spark-156 <lucabergman@hotmail.nl>
2021-01-28 17:10:22 +0100

    updated comments

--- server/server.js
+++ server/server.js
@@ -4,16 +4,15 @@ const morgan = require('morgan');
 
 const PORT = process.env.port || 5000;
 
-// create express app
 const app = express();
 
 // Logging middleware
 app.use(morgan('dev'));
 
-// parse application/x-www-form-urlencoded
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// parse application/json
+// Parse application/json
 app.use(bodyParser.json())
 
 // Import and init projects route
@@ -26,7 +25,6 @@ app.use((err, req, res, next) => {
   res.status(status).send(err.message);
 });
 
-// listen for requests
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
