d72840e5cc900daf3bce478126934d63c3b897b3
spark-156 <lucabergman@hotmail.nl>
2021-01-31 22:32:50 +0100

    🎨 moved swiper divs to reactJS

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 
-import { AboutSlide } from './components/AboutSlide'; 
+import { AboutSlide } from './components/AboutSlide';
+
 
 function App() {
   return (
-    <div>
-      <AboutSlide />
-    </div>
-  );
+    <div class="swiper-container background">
+      <div class="swiper-wrapper">
+        <AboutSlide />
+
+        <div class="swiper-button-next"></div>
+      </div>
+    </div>);
 }
 
 export default App;
