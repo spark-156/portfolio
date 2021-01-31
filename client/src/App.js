aa4652f3cb6fc34c9c60437293b6397a480bf7d2
spark-156 <lucabergman@hotmail.nl>
2021-01-31 23:11:57 +0100

    🎨 Reformat

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 
+import { Navbar } from './components/Navbar';
 import { AboutSlide } from './components/AboutSlide';
+import { LoadingSpinner } from './components/LoadingSpinner';
 
+import { useFetch } from './lib/apiHandler';
 
-function App() {
+export default function App() {
+  const [about, setAbout] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  const tempAbout = useFetch('/api/about/');
+
+  useEffect(() => {
+      if (tempAbout !== null) {
+        setAbout(tempAbout);
+        setLoading(false);
+      } 
+  }, [tempAbout]);
+
   return (
-    <div class="swiper-container background">
-      <div class="swiper-wrapper">
-        <AboutSlide />
+    <div class="background">
+      <Navbar about={about} loading={loading} />
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <AboutSlide about={about} loading={loading} />
 
-        <div class="swiper-button-next"></div>
+          <div class="swiper-button-next"></div>
+        </div>
       </div>
     </div>);
-}
-
-export default App;
+};
