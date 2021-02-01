bdeda624d9070cf4ebe61da051c9f9cee078f88a
spark-156 <lucabergman@hotmail.nl>
2021-02-01 17:19:40 +0100

    ✨ added mongooseDB and mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const fileuploader = require("express-fileupload");
+
+
+const db = require("./models/index");
+
+// Set up default mongodb connection
+
+
 
 const PORT = process.env.port || 5000;
 
@@ -15,6 +24,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // Parse application/json
 app.use(bodyParser.json())
 
+// Parse uploaded files
+app.use(fileuploader());
+
 // Import and init projects route
 const projectsRouter = require('./routes/projects');
 app.use('/api/projects', projectsRouter);
@@ -28,6 +40,19 @@ app.use((err, req, res, next) => {
   status = err.status || 500;
   res.status(status).send(err.message);
 });
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.error("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
