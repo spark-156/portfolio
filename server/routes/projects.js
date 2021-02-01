1356881815e69f62afff3ea6b9bcbdfac3c4ae49
spark-156 <lucabergman@hotmail.nl>
2021-02-01 22:37:40 +0100

    ✨ /api/projects/all made

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const app = express();
+const mongoose = require("mongoose");
+const ProjectsModel = mongoose.model("Projects");
 
 const projectsRouter = express.Router();
 
 // Hello World route
-projectsRouter.get('/all', (req, res, next) => {
-
+projectsRouter.get('/all', async (req, res, next) => {
+    try {
+        const projects = await ProjectsModel.find();
+        if (!projects.length > 0) {
+            return res.status(404).send("No projects found")
+        }
+        res.status(200).send(projects);
+    } catch {
+        const error = new Error("Could not get all projects");
+        next(error);
+    }
 });
 
 projectsRouter.get('/latest', (req, res, next) => {
@@ -13,7 +23,7 @@ projectsRouter.get('/latest', (req, res, next) => {
 });
 
 projectsRouter.post('/admin/new', (req, res, next) => {
-
+    
 })
 
 projectsRouter.put('/admin/id/:id', (req, res, next) => {
@@ -23,4 +33,5 @@ projectsRouter.put('/admin/id/:id', (req, res, next) => {
 projectsRouter.delete('/admin/id/:id', (req, res, next) => {
 
 })
+
 module.exports = projectsRouter;
