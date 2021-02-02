777a9056c3fec0c5f2a43ecf7e767a91adef210c
spark-156 <lucabergman@hotmail.nl>
2021-02-02 16:16:26 +0100

    🎉 Route based code splitting

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,29 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState, useEffect } from 'react';
+import React, { lazy, Suspense } from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-import { Navbar } from './components/Navbar';
-import { AboutPage } from './components/AboutPage';
+// pages
+const Home = lazy(() => import("./routes/Home"));
+const Admin = lazy(() => import("./routes/Admin"));
+const NotFoundPage = lazy(() => import("./routes/NotFoundPage"));
+const Projects = lazy(() => import("./routes/Projects"));
+const Contact = lazy(() => import("./routes/Contact"));
 
-import { useFetch } from './lib/apiHandler';
-
-// Ant Design imports
-import { Spin } from 'antd';
-import 'antd/dist/antd.css';
-
-export default function App() {
-  const [about, setAbout] = useState(null);
-  const [loading, setLoading] = useState({ loading: true });
-
-  const tempAbout = useFetch('/api/about/');
-
-  useEffect(() => {
-    if (tempAbout !== null) {
-      setAbout(tempAbout);
-      setLoading({ loading: false });
-    }
-  }, [tempAbout]);
-
-  const containerLoading = (<div class="background">
-    <Spin size="large" id="spinner" style={{position: "absolute", top: "50%", left: "50%"}}/>
-    </div>);
-
-
-  const containerLoaded = (<div class="background">
-    <Navbar about={about} />
-    <AboutPage about={about} />
-  </div>);
-
-  return loading.loading ? containerLoading : containerLoaded;
+const App = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/admin" component={Admin} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/Contact" component={Contact} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Router>
+    </Suspense>
+  );
 };
+
+export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
 
 export function Navbar({ about }) {
-    return (<div id="navbar">
-            <div id="navbarLeft">{about.name}</div>
-            <div id="navbarRight">
-                <button>Projects</button>
-                <button>Contact</button>
-            </div>
-        </div>)
+    return (
+        <nav className="nav-container">
+            <ul className="nav-unordered-list">
+                <li className="nav-list-item">
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li className="nav-list-item">
+                    <NavLink to="/Admin">Admin</NavLink>
+                </li>
+                <li className="nav-list-item">
+                    <NavLink to="/Projects">Projects</NavLink>
+                </li>
+                <li className="nav-list-item">
+                    <NavLink to="/Projects">Contact</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 }
