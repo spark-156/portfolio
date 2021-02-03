8cc8faee9ae3e10605167526612edde62cf5041a
spark-156 <lucabergman@hotmail.nl>
2021-02-03 10:21:08 +0100

    ✨ Projects tab INIT

--- client/src/routes/Projects.js
+++ client/src/routes/Projects.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Space, Card, Image, Spin, Row, Col } from 'antd';
 
-export default function Projects() {
-    return <div id="Projects"></div>
+export default function Projects({ isMobile, mobileWidth, setLoading, loading }) {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const getProjects = () => {
+            fetch('/api/projects/all')
+                .then(res => { return res.json() })
+                .then(projects => { setProjects(projects) })
+        }
+        // const getImages = () => {
+        //     fetch('/api/images/all')
+        //         .then(res => { return res.json() })
+        //         .then(images => { setImages(images); setLoading(false) })
+        // }
+        getProjects();
+        // getImages();
+    }, []);
+
+    return loading ? <Spin id="loading"/> : (
+        <Space id="projects">
+            
+        </Space>
+    )
 }
