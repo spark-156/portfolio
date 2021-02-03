f79e84b763c84ea5c2040ed7847de3cfc7b394b0
spark-156 <lucabergman@hotmail.nl>
2021-02-03 08:53:55 +0100

    💄 Added mobile and desktop resizing, added home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import MediaQuery, { useMediaQuery }  from 'react-responsive'
+import { Row, Col } from 'antd';
 
 import Navbar from "./components/Navbar";
+import 'antd/dist/antd.css';
+
 import './App.css';
 
 // pages
@@ -11,13 +15,36 @@ const NotFoundPage = lazy(() => import("./routes/NotFoundPage"));
 const Projects = lazy(() => import("./routes/Projects"));
 const Contact = lazy(() => import("./routes/Contact"));
 
+
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const mobileWidth = 1200;
+  
+  const isMobile = useMediaQuery({
+    query: `(min-device-width: ${mobileWidth})`
+  })
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Router>
-        <Navbar />
+        <Navbar mobileWidth={mobileWidth} />
         <Switch>
-          <Route path="/" component={Home} exact />
+          <Route path="/" exact >
+            <div>
+              <MediaQuery maxDeviceWidth={mobileWidth}>
+                <Row id="Home">
+                  <Col span={24}><Home isMobile={isMobile} mobileWidth={mobileWidth} loading={loading} setLoading={setLoading} /></Col>
+                </Row>
+              </MediaQuery>
+              <MediaQuery minDeviceWidth={mobileWidth}>
+                <Row id="Home">
+                  <Col span={4} />
+                  <Col span={16}><Home isMobile={isMobile} mobileWidth={mobileWidth} loading={loading} setLoading={setLoading} /></Col>
+                  <Col span={4} />
+                </Row>
+              </MediaQuery>
+            </div>
+          </Route>
           <Route path="/admin" component={Admin} />
           <Route path="/projects" component={Projects} />
           <Route path="/Contact" component={Contact} />
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,5 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Image, Spin, Row, Col } from 'antd';
+import MediaQuery from 'react-responsive'
 
-export default function Home() {
-    return <div id="Home"></div>
+import './Home.css';
+import './Loading.css';
+
+
+
+export default function Home({ isMobile, mobileWidth, setLoading, loading }) {
+    const [images, setImages] = useState(null);
+    const [about, setAbout] = useState(null);
+    
+    //TODO fix crash upon switching to mobile or desktop size
+
+    useEffect(() => {
+        setLoading(true);
+        const getAbout = () => {
+            fetch('/api/about/latest')
+                .then(res => { return res.json() })
+                .then(about => { setAbout(about) })
+        }
+        const getImages = () => {
+            fetch('/api/images/all')
+                .then(res => { return res.json() })
+                .then(images => { setImages(images); setLoading(false) })
+        }
+        getAbout();
+        getImages();
+    }, [setLoading, isMobile]);
+
+    return loading ? <Spin size="large" id="loading" /> : (
+        <div>
+            <div id="name">{about[0].name}</div>
+            <div id="description">{about[0].description}</div>
+            <MediaQuery minDeviceWidth={mobileWidth}>
+                <Image.PreviewGroup>
+                    <Row id="imageContainer">
+                        {images.slice(-4).map(image => { return <Col span={6}><Image width="100%" height="100%" alt={image.alt} src={`/api/images/id/${image._id}`} /></Col> })}
+                    </Row>
+                </Image.PreviewGroup>
+            </MediaQuery>
+            <MediaQuery maxDeviceWidth={mobileWidth}>
+                <Row id="imageContainer">
+                    <Col span={24}><Image width="100%" height="100%" alt={images[0].alt} src={`/api/images/id/${images[0]._id}`} /></Col>
+                </Row>
+            </MediaQuery>
+        </div>
+    )
 }
