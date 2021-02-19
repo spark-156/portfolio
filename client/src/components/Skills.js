b4adee772ec39e02e596a574b587a77a9ecca2a6
spark-156 <lucabergman@hotmail.nl>
2021-02-19 17:28:18 +0100

    ✨ Static Skills

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Space, Image, Spin, Row, Col } from 'antd';
-
-import "./Skills.css";
-
-const { Meta } = Card;
+import { Spin, Row, Col } from 'antd';
 
 export default function Projects({ setLoading, loading }) {
-    const [skills, setSkills] = useState([{ skill: "Javascript", percentage: 50 }, { skill: "Python", percentage: 50 }]);
+    const [skills, setSkills] = useState([{ skill: "Javascript", percentage: 5 }, { skill: "Python", percentage: 70 }, { skill: "HTML", percentage: 30 }, { skill: "CSS", percentage: 80 }]);
+
+    const backgroundColors = ["#DD0100", "#225095", "#FAC901"];
 
     useEffect(() => {
         const getSkills = () => {
@@ -17,15 +15,18 @@ export default function Projects({ setLoading, loading }) {
         // getSkills();
     }, []);
 
-    //TODO import and use swiper instead of horizontal scrolling
-
     return loading ? <Spin id="loading" /> : (
-        <div id="skills" className="blackBorder">
-            {skills.map(skill => {
+        <div id="skills" style={{color: "#ffffff"}} className="blackBorder">
+            {skills.map((skill, index) => { 
                 return (
-                    <div>
-                        {skill.skill}: {skill.percentage}%
-                    </div>
+                    <Row gutter={10}>
+                        <Col span={10}>
+                            <div className="skillsName">{skill.skill}</div>
+                        </Col>
+                        <Col span={14}>
+                            <span className="skillsBar" style={{backgroundColor: backgroundColors[index % backgroundColors.length], width: `${skill.percentage}%`, display: "block", height: "95%"}}/>
+                        </Col>
+                    </Row>
                 )
             })}
         </div>
