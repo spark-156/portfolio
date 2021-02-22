0dcbde05bcf9650a6386d18a3b0fcfe579cfd8d7
spark-156 <lucabergman@hotmail.nl>
2021-02-22 17:32:50 +0100

    ✨ Admin register POST path

--- server/models/admin.model.js
+++ server/models/admin.model.js
@@ -9,24 +9,6 @@ module.exports = mongoose => {
             { timestamps: true }
         )
     );
-    Admin.pre('save', function(next) {
-        // Check if document is new or a new password has been set
-        if (this.isNew || this.isModified('password')) {
-          // Saving reference to this because of changing scopes
-          const document = this;
-          bcrypt.hash(document.password, saltRounds,
-            function(err, hashedPassword) {
-            if (err) {
-              next(err);
-            }
-            else {
-              document.password = hashedPassword;
-              next();
-            }
-          });
-        } else {
-          next();
-        }
-      });
+  
     return Admin;
 };
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,9 +1,42 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
 const mongoose = require("mongoose");
 const AdminModel = mongoose.model("Admin");
 
+AdminModel.schema.pre('save', function(next) {
+    // Check if document is new or a new password has been set
+    if (this.isNew || this.isModified('password')) {
+      // Saving reference to this because of changing scopes
+      const document = this;
+      bcrypt.hash(document.password, saltRounds,
+        function(err, hashedPassword) {
+        if (err) {
+          next(err);
+        }
+        else {
+          document.password = hashedPassword;
+          next();
+        }
+      });
+    } else {
+      next();
+    }
+  });
+
 const adminRouter = express.Router();
 
-adminRouter.get()
+adminRouter.post('/register', (req, res, next) => {
+    const { email, password} = req.body;
+    const admin = new AdminModel({ email, password});
+    console.log("saving admin");
+    admin.save(err => {
+        if (err) {
+            res.status(500).send("Error registering new admin");
+        } else {
+            res.status(200).send("Admin registered");
+        }
+    });
+});
 
 module.exports = adminRouter;
