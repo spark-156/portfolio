ad896cce4617f911a7f17a83af16cfe301c55569
spark-156 <lucabergman@hotmail.nl>
2021-02-23 18:11:17 +0100

    ✨ /api/skills latest and /id/:id call

---
+++ server/routes/skills.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const SkillsModel = mongoose.model("Skills");
+
+const basicAuth = require('../authorization.js');
+
+const skillsRouter = express.Router();
+
+skillsRouter.use('/id/:id', async (req, res, next) => {
+    try {
+        const skill = await SkillsModel.findById(req.params.id);
+        if (!skill > 0) return res.status(404).send("No skill found for this id");
+        req.foundSkill = skill;
+        next();
+    } catch {
+        next(new Error("could not get specific skills by id"));
+    }
+});
+
+skillsRouter.get('/id/:id', async (req, res, next) => {
+    // Get a specific skills by id
+    try {
+        res.send(req.foundSkill);
+    } catch {
+        next(new Error("could not get specific skills by id"));
+    }
+});
+
+skillsRouter.get('/latest', async (req, res, next) => {
+    try {
+        const skill = await SkillsModel.find().limit(1).sort({$natural:-1});
+        if (!skill > 0) return res.status(404).send("No skill found for this id");
+        res.send(skill);
+    } catch {
+        next(new Error("Could not get latest skills"));
+    }
+});
+
+skillsRouter.post('/new', basicAuth, async (req, res, next) => {
+    try {
+        // TODO make this
+    } catch {
+        next(new Error("Could not create new skills object"));
+    }
+});
