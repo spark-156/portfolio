187f993c29e3b3d62e0b5e5c45ed8d65973dcf00
spark-156 <lucabergman@hotmail.nl>
2021-02-24 16:07:17 +0100

    ✨ Skills now calls api

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { Spin, Row, Col } from 'antd';
 
-export default function Projects({ setLoading, loading }) {
-    const [skills, setSkills] = useState([{ skill: "Javascript", percentage: 5 }, { skill: "Python", percentage: 70 }, { skill: "HTML", percentage: 30 }, { skill: "CSS", percentage: 80 }]);
-
+export default function skills() {
+    const [skills, setSkills] = useState([]);
+    const [loading, setLoading] = useState(true)
     const backgroundColors = ["#DD0100", "#225095", "#FAC901"];
 
     useEffect(() => {
         const getSkills = () => {
-            fetch('/api/skills/all')
+            fetch('/api/skills/latest')
                 .then(res => { return res.json() })
-                .then(skills => { setSkills(skills); setLoading(false) })
+                .then(skills => { setSkills(skills[0].skills); setLoading(false) })
         }
-        // getSkills();
+        getSkills();
     }, []);
 
     return loading ? <Spin id="loading" /> : (
         <div id="skills" style={{color: "#ffffff", backgroundColor: "#000000" }} className="blackBorder">
             {skills.map((skill, index) => { 
                 return (
-                    <Row gutter={10}>
+                    <Row key={`skill${index}`} gutter={10}>
                         <Col span={10}>
-                            <div className="skillsName">{skill.skill}</div>
+                            <div className="skillsName">{skill.skillName}</div>
                         </Col>
                         <Col span={14}>
-                            <span className="skillsBar" style={{backgroundColor: backgroundColors[index % backgroundColors.length], width: `${skill.percentage}%`, display: "block", height: "95%"}}/>
+                            <span className="skillsBar" style={{backgroundColor: backgroundColors[index % backgroundColors.length], width: `${skill.skillGrade}%`, display: "block", height: "95%"}}/>
                         </Col>
                     </Row>
                 )
