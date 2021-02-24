ce86f89636503e487b7a1d7a2dbf12b13ed95fad
spark-156 <lucabergman@hotmail.nl>
2021-02-24 16:09:52 +0100

    🐛 Fix eslint errors

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -9,10 +9,10 @@ export default function Contact() {
         <section id="contact" className="blackBorder">
             {/* Change email address to a custom one */}
             <div id="contactCenter">
-                <div className="contactDetails"><MailOutlined /> <a className="contactLink" href="mailto:" target="_blank" rel="noreferrer">Email</a></div>
-                <div className="contactDetails"><GithubOutlined /> <a className="contactLink" href="https://github.com/spark-156" target="_blank" rel="noreferrer">github</a></div>
-                <div className="contactDetails"><LinkedinOutlined /> <a className="contactLink" href="https://www.linkedin.com/in/luca-bergman-30b28b203/" target="_blank" rel="noreferrer">LinkedIn</a></div>
-                <div className="contactDetails"><InstagramOutlined /> <a className="contactLink" href="https://www.instagram.com/lucaisawesome_/" target="_blank" rel="noreferrer">Instagram</a></div>
+                <div className="contactDetails"><MailOutlined /> <a className="contactLink" href="mailto:" target="_blank" rel="noreferrer noopener">Email</a></div>
+                <div className="contactDetails"><GithubOutlined /> <a className="contactLink" href="https://github.com/spark-156" target="_blank" rel="noreferrer noopener">github</a></div>
+                <div className="contactDetails"><LinkedinOutlined /> <a className="contactLink" href="https://www.linkedin.com/in/luca-bergman-30b28b203/" target="_blank" rel="noreferrer noopener">LinkedIn</a></div>
+                <div className="contactDetails"><InstagramOutlined /> <a className="contactLink" href="https://www.instagram.com/lucaisawesome_/" target="_blank" rel="noreferrer noopener">Instagram</a></div>
             </div>
         </section>
     )
