65a11175283775861797c43333df41770d401671
spark-156 <lucabergman@hotmail.nl>
2021-02-27 03:56:36 +0100

    ✨ 404 no projects found

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -30,7 +30,7 @@ export default function Projects({ isAdmin }) {
     // TODO update Spin loading to better fit inside the painting
     return loading ? <Spin id="loading" /> : (
         <>
-            <section id="projectsList" className="blackBorder">
+            {projects.length > 0 ? (<section id="projectsList" className="blackBorder">
                 {swiper ? (
                     <ul>
                         {projects.map((project, index) => {
@@ -47,7 +47,7 @@ export default function Projects({ isAdmin }) {
                         })}
                     </ul>
                 ) : <div>Loading projects</div>}
-            </section>
+            </section>) : <section id="projectsList" className="blackBorder" >No projects fpimd</section>}
             <Swiper
                 style={{ width: "100%", height: "100%" }}
                 id="projects"
@@ -62,7 +62,7 @@ export default function Projects({ isAdmin }) {
                 onInit={swiperInstance => { setSwiper(swiperInstance) }}
                 onSlideChange={(swiper) => { setActiveProjectIndex(swiper.realIndex); }}
             >
-                {projects.map((project, index) => {
+                {projects.length > 0 ? (projects.map((project, index) => {
                     return (
                         <SwiperSlide key={`project${index}`}>
                             <Row className="projectsTop">
@@ -85,7 +85,19 @@ export default function Projects({ isAdmin }) {
                             </Row>
                         </SwiperSlide>
                     )
-                })}
+                })) : (<SwiperSlide>
+                    <Row className="projectsTop">
+                        <Col className="projectsTopCol projectsText">
+                            <div>No projects found, please check again later!</div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="projectsBottom">
+
+                        </Col>
+                    </Row>
+                </SwiperSlide>)
+                }
             </Swiper>
         </>
     )
