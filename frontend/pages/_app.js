64595c34749dc36d3692c605d2de6cfcaa65dee2
spark-156 <lucabergman@hotmail.nl>
2021-05-05 16:51:35 +0200

    ✨ fix swiper by importing necessary sass files

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,6 +5,11 @@ import '../styles/About.css'
 import '../styles/Projects.css'
 import '../styles/loading.css'
 
+import 'swiper/swiper.scss';
+import 'antd/dist/antd.css';
+import 'swiper/components/autoplay';
+import 'swiper/components/effect-cube/effect-cube.scss';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
