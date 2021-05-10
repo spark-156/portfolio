c79c9e370634a154f7cc2492b232fd1763def1cd
spark-156 <lucabergman@hotmail.nl>
2021-05-10 11:47:47 +0200

    ✏️ fix capitalization

--- frontend/components/Contact.js
+++ frontend/components/Contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
             {/* Change email address to a custom one */}
             <div id="contactCenter">
                 <div className="contactDetails"><MailOutlined /> <a className="contactLink" href="mailto:luca.bergman@student.hu.nl" target="_blank" rel="noreferrer noopener">Email</a></div>
-                <div className="contactDetails"><GithubOutlined /> <a className="contactLink" href="https://github.com/spark-156" target="_blank" rel="noreferrer noopener">github</a></div>
+                <div className="contactDetails"><GithubOutlined /> <a className="contactLink" href="https://github.com/spark-156" target="_blank" rel="noreferrer noopener">Github</a></div>
                 <div className="contactDetails"><LinkedinOutlined /> <a className="contactLink" href="https://www.linkedin.com/in/luca-bergman-30b28b203/" target="_blank" rel="noreferrer noopener">LinkedIn</a></div>
                 <div className="contactDetails"><InstagramOutlined /> <a className="contactLink" href="https://www.instagram.com/lucaisawesome_/" target="_blank" rel="noreferrer noopener">Instagram</a></div>
             </div>
