008d0a4e4e6652547936d4991234e0c9f07a58b2
spark156 <lucabergman@hotmail.nl>
2021-11-20 13:04:58 +0100

    implement react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import { Index } from './components/Index';
+import { Projects } from './components/Projects';
+import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
+import { NotFound } from './components/NotFound';
 
 function App() {
-  return <div>Hello World!</div>
+  return <Router>
+    <Routes>
+      <Route path='*' element={<NotFound />} />
+      <Route path="/" element={<Index />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>
 }
 
 export default App;
