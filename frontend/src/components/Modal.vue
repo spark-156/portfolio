ad3345805bdf895fca302dbf52e640c030fc8ab7
spark156 <lucabergman@hotmail.nl>
2022-01-18 16:22:39 +0100

    update Modal

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -1,106 +1,44 @@
 <template>
-  <transition name="modal">
     <div class="modal-mask">
-      <div class="modal-wrapper">
-        <div class="modal-container">
-
-          <div class="modal-header">
-            <slot name="header">
-              default header
-            </slot>
-          </div>
-
-          <div class="modal-body">
-            <slot name="body">
-              default body
-            </slot>
-          </div>
-
-          <div class="modal-footer">
-            <slot name="footer">
-              default footer
-              <button class="modal-default-button" @click="$emit('close')">
-                OK
-              </button>
-            </slot>
-          </div>
-        </div>
+      <div class="content">
+        <slot></slot>
       </div>
+
+      <button-vue :onClick="() => $emit('close')" :color="'purple'">Close</button-vue>
     </div>
-  </transition>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import ButtonVue from './Button.vue'
 
 export default defineComponent({
-  name: 'Modal'
+  name: 'Modal',
+  components: {
+    'button-vue': ButtonVue
+  }
 })
 </script>
 
 <style lang="scss" scoped>
 .modal-mask {
   position: fixed;
-  z-index: 9998;
+  z-index: 999;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
+  background-color: $colors-white;
+
+  display: grid;
+  place-items: center;
+
 }
 
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
+.content {
+  display: grid;
+  place-items: center;
+  grid-auto-rows: 1fr;
+  gap: $lengths-1;
 }
-
-.modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
-  font-family: Helvetica, Arial, sans-serif;
-}
-
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
-
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
-}
-
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
-.modal-enter {
-  opacity: 0;
-}
-
-.modal-leave-active {
-  opacity: 0;
-}
-
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
-
 </style>
