70e2509638218f36443a819254ff0d3591de9c95
spark156 <lucabergman@hotmail.nl>
2022-01-18 14:34:56 +0100

    create link componenet

---
+++ frontend/src/components/Link.vue
@@ -0,0 +1,25 @@
+<template>
+  <a :href="href">
+    <slot></slot>
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Link',
+  props: {
+    href: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+a {
+  color: inherit;
+}
+</style>
