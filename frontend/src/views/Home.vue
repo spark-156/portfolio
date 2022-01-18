a7ea945cb17b20cf5c28f2232eecb784e4dc6d3b
spark156 <lucabergman@hotmail.nl>
2022-01-18 16:36:19 +0100

    refactor skills to its own component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,16 +6,8 @@
     And I am a pasionate full stack developer
   </colored-background-div-vue>
 
-  <image-vue :src="'https://www.bostonmagazine.com/wp-content/uploads/sites/2/2021/08/rubber-duck-stock-t.jpg'" />
-  <colored-background-div-vue :color="'purple'">
-    Fluent in:
-    <link-vue href="/skills/docker">Docker</link-vue>
-    <link-vue href="/skills/linux">Linux</link-vue>
-    <link-vue href="/skills/python">Python</link-vue>
-    <link-vue href="/skills/javascript">Javascript</link-vue>
-    &amp;
-    <link-vue href="/skills/typescript">Typescript</link-vue>
-  </colored-background-div-vue>
+  <list-of-skills-vue></list-of-skills-vue>
+
   <image-vue :src="'/secondaryfoto.jpeg'" />
   <colored-background-div-vue :color="'yellow'">
     Some of my hobby projects you can check out:
@@ -31,13 +23,15 @@ import { defineComponent } from 'vue'
 import ImageVue from '@/components/Image.vue'
 import ColoredBackgroundDiv from '@/components/ColoredBackgroundDiv.vue'
 import LinkVue from '@/components/Link.vue'
+import ListOfSkillsVue from '@/components/ListOfSkills.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
     'image-vue': ImageVue,
     'colored-background-div-vue': ColoredBackgroundDiv,
-    'link-vue': LinkVue
+    'link-vue': LinkVue,
+    'list-of-skills-vue': ListOfSkillsVue
   }
 })
 </script>
