b2faf6f3e625bb0e1e10448964907776cd4f9152
spark156 <lucabergman@hotmail.nl>
2022-01-19 10:21:31 +0100

    remove unused routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,29 +8,6 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: () => import('../views/Contact.vue')
-  },
-  {
-    path: '/skills',
-    name: 'Skills',
-    component: () => import('../views/Skills.vue')
-  },
-  {
-    path: '/projects',
-    name: 'Projects',
-    component: () => import('../views/Projects.vue')
-  },
-  // {
   //   path: '/projects/:id',
   //   name: 'Project',
   //   component: () => import('../views/Project.vue')
