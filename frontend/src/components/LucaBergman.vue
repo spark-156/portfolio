8bf985deb8e4d418b58e19f711ee093fc49f705c
spark156 <lucabergman@hotmail.nl>
2022-01-20 11:39:30 +0100

    create lucabergman component

---
+++ frontend/src/components/LucaBergman.vue
@@ -0,0 +1,22 @@
+<template>
+  <image-vue :src="'/portfoliofoto.jpg'" />
+  <colored-background-div-vue id="lucabergman" :color="'blue'">
+    My name is Luca Bergman
+    <br />
+    I am a passionate full stack developer
+  </colored-background-div-vue>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ColoredBackgroundDivVue from './ColoredBackgroundDiv.vue'
+import ImageVue from './Image.vue'
+
+export default defineComponent({
+  name: 'LucaBergman',
+  components: {
+    'colored-background-div-vue': ColoredBackgroundDivVue,
+    'image-vue': ImageVue
+  }
+})
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,5 @@
 <template>
-  <image-vue :src="'/portfoliofoto.jpg'" />
-  <colored-background-div-vue id="lucabergman" :color="'blue'">
-    My name is Luca Bergman
-    <br />
-    I am a passionate full stack developer
-  </colored-background-div-vue>
+  <luca-bergman-vue></luca-bergman-vue>
 
   <list-of-skills-vue></list-of-skills-vue>
 
@@ -15,17 +10,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import ImageVue from '@/components/Image.vue'
-import ColoredBackgroundDiv from '@/components/ColoredBackgroundDiv.vue'
 import ListOfSkillsVue from '@/components/ListOfSkills.vue'
 import ListOfProjectsVue from '@/components/ListOfProjects.vue'
 import ContactDetailsVue from '@/components/ContactDetails.vue'
+import LucaBergmanVue from '@/components/LucaBergman.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-    'image-vue': ImageVue,
-    'colored-background-div-vue': ColoredBackgroundDiv,
+    'luca-bergman-vue': LucaBergmanVue,
     'list-of-projects-vue': ListOfProjectsVue,
     'list-of-skills-vue': ListOfSkillsVue,
     'contact-details': ContactDetailsVue
